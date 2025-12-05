4e5f7089736f1386f1dea30e8b9cc63ae336c742
TheBedDev <240914167+thebeddev-code@users.noreply.github.com>
2025-12-05 12:05:08 +0500

    feat(utils): implement calc radians from [hours, degrees] util

---
+++ app/lib/math.ts
@@ -0,0 +1,12 @@
+import { types } from "node:util";
+
+export function calcRadiansFrom(
+  value: number,
+  type: "hours" | "degrees" = "degrees"
+) {
+  let degrees = value;
+  if (type == "hours") degrees = value * 30;
+  // 360/2*Math.Pi = degrees/x
+  // using cross multiplication to solve the proportion
+  return (degrees * 2 * Math.PI) / 360;
+}
