1c437c9192b7749847152aeea5720b8ee43a7e1d
TheBedDev <240914167+thebeddev-code@users.noreply.github.com>
2025-12-10 18:11:55 +0500

    feat(landing): render todo list on the left

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { ClockGraph } from "./components/ClockGraph";
+import { TodoList } from "./components/TodoList";
 
 export default function Home() {
   return (
-    <main className="bg-white flex justify-center items-center h-dvh w-dvw">
-      <ClockGraph />
+    <main className="bg-white grid grid-cols-[40%_1fr] gap-20 justify-center items-center h-dvh w-dvw px-10">
+      <TodoList />
+      <div className="flex max-w-full">
+        <ClockGraph />
+      </div>
     </main>
   );
 }
