09d0417ea756356e5f5aba54b05006d9eeed3162
TheBedDev <240914167+thebeddev-code@users.noreply.github.com>
2025-12-12 13:31:27 +0500

    feat(tests): vitest config, starter home page test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,14 @@ import { TodoList } from "./components/TodoList";
 
 export default function Home() {
   return (
-    <main className="bg-white grid grid-cols-[40%_1fr] gap-20 justify-center items-center h-dvh w-dvw px-10">
-      <TodoList />
-      <div className="flex max-w-full">
-        <ClockGraph />
-      </div>
-    </main>
+    <div>
+      <h1>Your todos</h1>
+      <main className="bg-white grid grid-cols-[40%_1fr] gap-20 justify-center items-center h-dvh w-dvw px-10">
+        <TodoList />
+        <div className="flex max-w-full">
+          <ClockGraph />
+        </div>
+      </main>
+    </div>
   );
 }
