4be93898549e7af2d68489898d78dc115b724bac
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2025-12-30 15:07:29 +0500

    refactor(graphs/ClockHandle): minor ui/ux changes

--- apps/web/src/features/graphs/components/ClockHandle.tsx
+++ apps/web/src/features/graphs/components/ClockHandle.tsx
@@ -87,12 +87,12 @@ export function ClockHandle({
             width: `${HANDLE_BUTTON_SIZE_PX}px`,
             height: `${HANDLE_BUTTON_SIZE_PX}px`,
           }}
-          className="bg-slate-900 rounded-full cursor-grab"
+          className="absolute bg-slate-800 rounded-full cursor-grab"
         />
-        <div className="border-slate-900 border border-dotted h-[50%] w-full" />
-        <div className="bg-slate-900 h-2 w-2 rounded-full cursor-grab" />
+        <div className="border-slate-800 border border-dotted h-[50%] w-1/2" />
+        {/* <div className="bg-slate-900 h-2 w-2 rounded-full cursor-grab" /> */}
       </div>
-      <div className="bg-slate-900 absolute h-2 w-2 rounded-full" />
+      <div className="bg-slate-800 absolute h-2 w-2 rounded-full" />
       {children}
     </div>
   );
