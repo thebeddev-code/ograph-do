4978c77a7efe0bebae4364d92fa3530ea59926ea
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2025-12-30 15:07:02 +0500

    refactor(graphs/Clock): draw hour markers

--- apps/web/src/features/graphs/components/Clock.tsx
+++ apps/web/src/features/graphs/components/Clock.tsx
@@ -5,6 +5,8 @@ interface Props {
 }
 
 export function Clock({ canvasRef }: Props) {
+  const hourMarkers = [];
+  for (let i = 1; i <= 12; ++i) hourMarkers[i] = i;
   return (
     <div className="relative w-[400px] h-[400px] rounded-full overflow-hidden">
       <canvas className="w-full h-full" ref={canvasRef} />
@@ -12,20 +14,29 @@ export function Clock({ canvasRef }: Props) {
         {Array.from({ length: 48 }, (_, i) => (
           <div
             key={i}
-            className="absolute w-px h-[5px] bg-red-50"
+            className="absolute w-px h-[5px] bg-slate-400"
             style={{
               transform: `rotate(${i * 7.5}deg) translateY(-180px)`,
             }}
           />
         ))}
-        {Array.from({ length: 12 }, (_, i) => (
+        {hourMarkers.map((h, i) => (
           <div
+            className="absolute"
             key={i}
-            className="absolute w-[2px] h-2 rounded-2xl bg-slate-600"
             style={{
               transform: `rotate(${i * 30}deg) translateY(-180px)`,
             }}
-          />
+          >
+            <div
+              className="bg-white text-slate-800 select-none"
+              style={{
+                rotate: `-${i * 30}deg`,
+              }}
+            >
+              {h}
+            </div>
+          </div>
         ))}
       </div>
     </div>
