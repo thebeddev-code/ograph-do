c007bc3f8a8cd5be5e0d51acc143b81c05f5a45f
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2025-12-30 17:06:26 +0500

    feat(mocks): todo generator fn

--- apps/web/src/testing/mocks/dataGenerators.ts
+++ apps/web/src/testing/mocks/dataGenerators.ts
@@ -1,21 +1,64 @@
 import { Todo } from "@/types/api";
-import { randSoonDate, randTodo, randText } from "@ngneat/falso";
+import { randNumber, randTodo, randText, randHex } from "@ngneat/falso";
 import { set } from "date-fns";
 
-export const mockTodos: Todo[] = [
-  {
-    id: 1,
-    title:
-      "Take a break from gaming: Disconnect and recharge your energy for the next adventure.",
-    description: randText({ maxCharCount: 500 }),
-    tags: ["shopping", "urgent"],
-    color: "#ffcc00",
-    status: "in-progress",
-    priority: "high",
-    startsAt: set(new Date(), { hours: 8, minutes: 30 }).toISOString(),
-    due: set(new Date(), { hours: 10, minutes: 30 }).toISOString(), // Valid future due date
-    createdAt: new Date().toISOString(), // Valid creation date
-    updatedAt: new Date().toISOString(), // Updated after creation
+let id = 0;
+export function createTodo(overrides: Partial<Todo>): Todo {
+  const base = randTodo(); // has title, status, priority, etc.[web:45]
+  const now = new Date();
+  const startAtHours = overrides.startsAt
+    ? new Date(overrides.startsAt).getHours()
+    : randNumber({
+        min: 0,
+        max: 22,
+      });
+
+  const startAtMinutes = overrides.startsAt
+    ? new Date(overrides.startsAt).getMinutes()
+    : randNumber({
+        min: 0,
+        max: 50,
+      });
+
+  const startsAt = set(now, {
+    hours: startAtHours,
+    minutes: startAtMinutes,
+    seconds: 0,
+    milliseconds: 0,
+  }).toISOString();
+
+  const due = set(now, {
+    hours: randNumber({ min: startAtHours + 1, max: 23 }),
+    minutes: randNumber({ min: startAtMinutes, max: 60 }),
+    seconds: 0,
+    milliseconds: 0,
+  }).toISOString();
+  const priorities = ["high", "medium", "low"] as const;
+  type Priority = (typeof priorities)[number]; // "high" | "medium" | "low"
+  const priority: Priority = priorities[randNumber({ min: 0, max: 2 })];
+  return {
+    id: ++id,
+    title: base.title,
+    description: randText({ charCount: randNumber({ min: 50, max: 500 }) }),
+    tags: randText({
+      length: randNumber({ min: 1, max: 10 }),
+      charCount: randNumber({ min: 4, max: 16 }),
+    }),
+    color: randHex(),
+    status: "pending",
+    priority,
+    startsAt,
+    due,
+    createdAt: now.toISOString(),
+    updatedAt: now.toISOString(),
     isRecurring: false,
-  },
+    ...overrides,
+  };
+}
+
+export const mockTodos: Todo[] = [
+  createTodo({
+    title: "Your todo",
+    startsAt: set(new Date(), { hours: 19, minutes: 0 }).toISOString(),
+  }),
 ];
