e0c2cd491ccb74131b8650a23917554bb39b6b73
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-01-03 13:04:44 +0500

    feat(todos/api): implement cr crcreate todo

---
+++ apps/web/src/features/todos/api/createTodo.ts
@@ -0,0 +1,76 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { api } from "@/lib/api-client";
+import { MutationConfig } from "@/lib/react-query";
+
+import { getTodosQueryOptions } from "./getTodos";
+import { Todo } from "@/types/api";
+import { CreateTodoPayload } from "@/lib/schemas/todo.schema";
+
+export const createTodo = (todoPayload: CreateTodoPayload) => {
+  return api.post(`/todos`, todoPayload);
+};
+
+type UseCreateTodoOptions = {
+  mutationConfig?: MutationConfig<typeof createTodo>;
+};
+
+export const useCreateTodo = ({ mutationConfig }: UseCreateTodoOptions) => {
+  const queryClient = useQueryClient();
+  const { onSuccess, onError, ...restConfig } = mutationConfig || {};
+  const queryKey = getTodosQueryOptions().queryKey;
+  return useMutation({
+    onSuccess: (...args) => {
+      queryClient.invalidateQueries({
+        queryKey,
+      });
+      onSuccess?.(...args);
+    },
+    onMutate: async (todoPayload, context) => {
+      // Cancel any outgoing refetches
+      // (so they don't overwrite our optimistic update)
+      await context.client.cancelQueries({ queryKey });
+
+      // Snapshot the previous value
+      const previousTodos = context.client.getQueryData(queryKey);
+
+      // Optimistically update to the new value
+      if (Array.isArray(previousTodos) && previousTodos.length > 0) {
+        context.client.setQueryData(queryKey, (cache) =>
+          cache
+            ? {
+                data: [
+                  ...previousTodos,
+                  { id: previousTodos.at(-1).id + 1, ...todoPayload },
+                ],
+                meta: cache.meta,
+              }
+            : cache,
+        );
+      } else {
+        context.client.setQueryData(queryKey, (cache) =>
+          cache
+            ? {
+                data: [{ ...(todoPayload as Todo), id: 1 }],
+                meta: cache.meta,
+              }
+            : cache,
+        );
+      }
+      // Return a result with the snapshotted value
+      return { previousTodos };
+    },
+    // If the mutation fails,
+    // use the result returned from onMutate to roll back
+    onError: (...args) => {
+      const [_, __, onMutateResult, context] = args;
+      context.client.setQueryData(
+        ["todos"],
+        (onMutateResult as { previousTodos: Todo[] })?.previousTodos,
+      );
+      onError?.(...args);
+    },
+    ...restConfig,
+    mutationFn: createTodo,
+  });
+};
