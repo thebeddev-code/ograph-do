2155cc8ba8f9e7114ba7f7233b8f7f6a4efff69c
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-01-13 16:44:57 +0500

    refactor(graphs): extract clock handle bts into its own comp

--- apps/web/src/features/graphs/components/ClockGraph/ClockGraph.tsx
+++ apps/web/src/features/graphs/components/ClockGraph/ClockGraph.tsx
@@ -10,9 +10,9 @@ import { addHours, formatDate, set } from "date-fns";
 import { Button } from "@/components/ui/button/button";
 import { calcClosestDistToClockHandle } from "../../utils/distToClockHandle";
 import { Sunrise, Sun, Sunset, Moon } from "lucide-react";
-import e from "express";
 import { ResetIcon } from "@radix-ui/react-icons";
 import { cn } from "@/utils/cn";
+import { ClockHandleTools } from "./ClockHandleTools";
 
 const RADIUS = 170;
 const MAX_LAST_CLICK_DIFF_MS = 300;
@@ -160,97 +160,11 @@ export function ClockGraph({ todos, onFormOpen }: Props) {
             setHasDraggedClockHandle(true);
           }}
           renderButtons={(stateSetters) => (
-            <div className="absolute">
-              <div
-                className="z-20 relative flex items-center justify-center w-28 h-28 mx-auto rounded-full"
-                onClick={(e) => e.stopPropagation()}
-              >
-                <button
-                  disabled={hasDraggedClockHandle}
-                  className={cn(
-                    `absolute 
-                    w-8 h-8 flex items-center justify-center text-gray-500 hover:text-slate-500
-                    bg-white/40 rounded-full border transition-colors duration-300`,
-                    {
-                      "opacity-40 hover:opacity-100": hasDraggedClockHandle,
-                    },
-                  )}
-                  onClick={(e) =>
-                    handleQuickTimeSwitchClick({
-                      stateSetters,
-                      resetClockHandle: true,
-                      event: e,
-                    })
-                  }
-                  title="Reset clock handle"
-                >
-                  <ResetIcon className="w-5 h-5" />
-                </button>
-                <button
-                  className="absolute translate-x-7 -translate-y-7 opacity-40 hover:opacity-100
-                    w-8 h-8 flex items-center justify-center text-gray-500 hover:text-slate-500
-                    bg-white/40 rounded-full border transition-colors duration-300"
-                  onClick={(e) =>
-                    handleQuickTimeSwitchClick({
-                      stateSetters,
-                      index: 1,
-                      event: e,
-                    })
-                  }
-                  title="Morning"
-                >
-                  <Sunrise className="w-5 h-5" />
-                </button>
-
-                <button
-                  className="absolute translate-7 opacity-40 hover:opacity-100
-                    w-8 h-8 flex items-center justify-center text-gray-500 hover:text-yellow-500
-                    bg-white/40 rounded-full border transition-colors duration-300"
-                  onClick={(e) =>
-                    handleQuickTimeSwitchClick({
-                      stateSetters,
-                      index: 2,
-                      event: e,
-                    })
-                  }
-                  title="Day"
-                >
-                  <Sun className="w-5 h-5" />
-                </button>
-
-                <button
-                  className="absolute -translate-x-7 translate-y-7 opacity-40 hover:opacity-100
-                    w-8 h-8 flex items-center justify-center text-gray-500 hover:text-purple-500
-                    bg-white/40 rounded-full border transition-colors duration-300"
-                  onClick={(e) =>
-                    handleQuickTimeSwitchClick({
-                      stateSetters,
-                      index: 3,
-                      event: e,
-                    })
-                  }
-                  title="Evening"
-                >
-                  <Sunset className="w-5 h-5" />
-                </button>
-
-                <button
-                  className="absolute -translate-7 opacity-40 hover:opacity-100
-                    w-8 h-8 flex items-center justify-center text-gray-500 hover:text-gray-800
-                    bg-white/40 rounded-full border transition-colors duration-300"
-                  onClick={(e) =>
-                    handleQuickTimeSwitchClick({
-                      stateSetters,
-                      index: 4,
-                      event: e,
-                    })
-                  }
-                  title="Night"
-                >
-                  <Moon className="w-5 h-5" />
-                </button>
-              </div>
-            </div>
+            <ClockHandleTools
+              stateSetters={stateSetters}
+              onQuickTimeSwitchClick={handleQuickTimeSwitchClick}
+              hasDraggedClockHandle={hasDraggedClockHandle}
+            />
           )}
         >
           {shouldTrackNewTodo && (
---
+++ apps/web/src/features/graphs/components/ClockGraph/ClockHandleTools.tsx
@@ -0,0 +1,114 @@
+import { ClockHandleStateSetters } from "./ClockHandle";
+import { Sunrise, Sun, Sunset, Moon } from "lucide-react";
+import { ResetIcon } from "@radix-ui/react-icons";
+import { cn } from "@/utils/cn";
+
+interface Props {
+  stateSetters: ClockHandleStateSetters;
+  hasDraggedClockHandle: boolean;
+  onQuickTimeSwitchClick: (args: {
+    stateSetters: ClockHandleStateSetters;
+    index?: number;
+    event: React.MouseEvent<HTMLButtonElement>;
+    resetClockHandle?: boolean;
+  }) => void;
+}
+export function ClockHandleTools({
+  hasDraggedClockHandle,
+  stateSetters,
+  onQuickTimeSwitchClick,
+}: Props) {
+  return (
+    <div className="absolute">
+      <div
+        className="z-20 relative flex items-center justify-center w-28 h-28 mx-auto rounded-full"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          disabled={hasDraggedClockHandle}
+          className={cn(
+            `absolute 
+                    w-8 h-8 flex items-center justify-center text-gray-500 hover:text-slate-500
+                    bg-white/40 rounded-full border transition-colors duration-300`,
+            {
+              "opacity-40 hover:opacity-100": hasDraggedClockHandle,
+            },
+          )}
+          onClick={(e) =>
+            onQuickTimeSwitchClick({
+              stateSetters,
+              resetClockHandle: true,
+              event: e,
+            })
+          }
+          title="Reset clock handle"
+        >
+          <ResetIcon className="w-5 h-5" />
+        </button>
+        <button
+          className="absolute translate-x-7 -translate-y-7 opacity-40 hover:opacity-100
+                    w-8 h-8 flex items-center justify-center text-gray-500 hover:text-slate-500
+                    bg-white/40 rounded-full border transition-colors duration-300"
+          onClick={(e) =>
+            onQuickTimeSwitchClick({
+              stateSetters,
+              index: 1,
+              event: e,
+            })
+          }
+          title="Morning"
+        >
+          <Sunrise className="w-5 h-5" />
+        </button>
+
+        <button
+          className="absolute translate-7 opacity-40 hover:opacity-100
+                    w-8 h-8 flex items-center justify-center text-gray-500 hover:text-yellow-500
+                    bg-white/40 rounded-full border transition-colors duration-300"
+          onClick={(e) =>
+            onQuickTimeSwitchClick({
+              stateSetters,
+              index: 2,
+              event: e,
+            })
+          }
+          title="Day"
+        >
+          <Sun className="w-5 h-5" />
+        </button>
+
+        <button
+          className="absolute -translate-x-7 translate-y-7 opacity-40 hover:opacity-100
+                    w-8 h-8 flex items-center justify-center text-gray-500 hover:text-purple-500
+                    bg-white/40 rounded-full border transition-colors duration-300"
+          onClick={(e) =>
+            onQuickTimeSwitchClick({
+              stateSetters,
+              index: 3,
+              event: e,
+            })
+          }
+          title="Evening"
+        >
+          <Sunset className="w-5 h-5" />
+        </button>
+
+        <button
+          className="absolute -translate-7 opacity-40 hover:opacity-100
+                    w-8 h-8 flex items-center justify-center text-gray-500 hover:text-gray-800
+                    bg-white/40 rounded-full border transition-colors duration-300"
+          onClick={(e) =>
+            onQuickTimeSwitchClick({
+              stateSetters,
+              index: 4,
+              event: e,
+            })
+          }
+          title="Night"
+        >
+          <Moon className="w-5 h-5" />
+        </button>
+      </div>
+    </div>
+  );
+}
